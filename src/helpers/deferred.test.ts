e2fbe5167e2d153b069b619f9f90d02b31ebcbb3
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-12-21 13:07:16 -0600

    improved deferred reject assertion

--- src/helpers/deferred.test.ts
+++ src/helpers/deferred.test.ts
@@ -10,6 +10,5 @@ test("reject", async () => {
   const deferred = createDeferred()
   const error = new Error("oops")
   setTimeout(() => deferred.reject(error))
-  const caught = await Promise.resolve(deferred).catch((error) => error)
-  expect(caught).toBe(error)
+  await expect(deferred).rejects.toThrow(error)
 })
