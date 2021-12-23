f6e75d52a28fd2ddd3e0f50cb59babe4d5888833
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-12-23 13:13:31 -0600

    fix imports i was being dumb

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
-// would normally use .js extensions here,
-// but that breaks in vitest
-// https://github.com/vitest-dev/vitest/issues/282
 /* eslint-disable import/no-unused-modules */
-export * from "./components/action-row"
-export * from "./components/button"
-export * from "./components/embed"
-export * from "./components/embed-field"
-export * from "./components/text"
-export * from "./root"
+export * from "./components/action-row.jsx"
+export * from "./components/button.jsx"
+export * from "./components/embed-field.jsx"
+export * from "./components/embed.jsx"
+export * from "./components/text.jsx"
+export * from "./root.js"
