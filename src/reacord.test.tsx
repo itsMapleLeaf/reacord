e4a46cfb5177360bd75d0f392a61a819d8103078
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-12-25 13:17:57 -0600

    remove unneeded deps

--- src/reacord.test.tsx
+++ src/reacord.test.tsx
@@ -1,4 +1,4 @@
-import "dotenv/config.js"
+import "dotenv/config"
 import { getEnvironmentValue } from "./helpers/get-environment-value.js"
 
 const testBotToken = getEnvironmentValue("TEST_BOT_TOKEN")
