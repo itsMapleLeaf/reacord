4cf9049496bb1b53c06e7ad977d8b00bc99de855
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-12-25 14:21:59 -0600

    placeholder test

--- src/reacord.test.tsx
+++ src/reacord.test.tsx
@@ -1,5 +1,8 @@
 import "dotenv/config"
+import { test } from "vitest"
 import { getEnvironmentValue } from "./helpers/get-environment-value.js"
 
 const testBotToken = getEnvironmentValue("TEST_BOT_TOKEN")
 const testChannelId = getEnvironmentValue("TEST_CHANNEL_ID")
+
+test.todo("reacord")
