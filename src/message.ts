cee63892fe16139a6d93ba7ba40f16475b51c037
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-12-26 11:56:03 -0600

    more comprehensive message types

--- src/message.ts
+++ src/message.ts
@@ -3,23 +3,21 @@ import type { EmbedOptions } from "./embed/embed-options"
 export type MessageOptions = {
   content: string
   embeds: EmbedOptions[]
-  actionRows: Array<
-    Array<
-      | {
-          type: "button"
-          customId: string
-          label?: string
-          style?: "primary" | "secondary" | "success" | "danger"
-          disabled?: boolean
-          emoji?: string
-        }
-      | {
-          type: "select"
-          customId: string
-          // todo
-        }
-    >
-  >
+  actionRows: Array<Array<MessageButtonOptions | MessageSelectOptions>>
+}
+
+export type MessageButtonOptions = {
+  type: "button"
+  customId: string
+  label?: string
+  style?: "primary" | "secondary" | "success" | "danger"
+  disabled?: boolean
+  emoji?: string
+}
+
+export type MessageSelectOptions = {
+  type: "select"
+  customId: string
 }
 
 export type Message = {
