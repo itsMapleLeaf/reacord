1355aefe7a782e5b593585cd6b8ddb7d271842a1
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-12-31 13:48:25 -0600

    setup tailwind

--- packages/docs-new/src/entry.server.tsx
+++ packages/docs-new/src/entry.server.tsx
@@ -17,6 +17,15 @@ export async function render(url: string) {
   return /* HTML */ `
     <!DOCTYPE html>
     <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width,initial-scale=1" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@500&family=Rubik:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&display=swap"
+        rel="stylesheet"
+      />
+      <link rel="stylesheet" href="/src/tailwind.css" />
       ${renderToString(<>{headTags}</>)}
     </head>
     <body>
