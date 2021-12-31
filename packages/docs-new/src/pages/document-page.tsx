f603bdd7ba663ef247b203b58cc333b51c963344
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-12-31 15:14:59 -0600

    render markdown page

--- packages/docs-new/src/pages/document-page.tsx
+++ packages/docs-new/src/pages/document-page.tsx
@@ -1,7 +1,10 @@
+import GettingStarted from "../docs/getting-started.md"
+
 export function DocumentPage() {
   return (
     <>
       <h1>Docs</h1>
+      <GettingStarted />
     </>
   )
 }
--- packages/docs-new/vite.config.ts
+++ packages/docs-new/vite.config.ts
@@ -1,6 +1,21 @@
+import rehypePrism from "@mapbox/rehype-prism"
 import react from "@vitejs/plugin-react"
+import remarkFrontmatter from "remark-frontmatter"
 import { defineConfig } from "vite"
+import xdm from "xdm/rollup"
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    xdm({
+      remarkPlugins: [remarkFrontmatter],
+      rehypePlugins: [rehypePrism],
+    }),
+  ],
+  resolve: {
+    alias: {
+      // https://github.com/brillout/vite-plugin-mdx/issues/44#issuecomment-974540152
+      "react/jsx-runtime": "react/jsx-runtime.js",
+    },
+  },
 })
