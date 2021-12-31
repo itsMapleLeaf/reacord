9afb8e47d26480d9af63ed29a66741c4be437208
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2021-12-31 16:36:03 -0600

    use react 18 concurrent mode

--- packages/docs-new/src/entry.client.tsx
+++ packages/docs-new/src/entry.client.tsx
@@ -1,13 +1,18 @@
-import { hydrate } from "react-dom"
+import { createRoot } from "react-dom"
 import { HeadProvider } from "react-head"
 import { ReactLocation } from "react-location"
 import { App } from "./app"
 
 const location = new ReactLocation()
 
-hydrate(
+createRoot(document.querySelector("#app")!).render(
   <HeadProvider>
     <App location={location} />
   </HeadProvider>,
-  document.querySelector("#app"),
 )
+
+declare module "react-dom" {
+  export function createRoot(element: Element): {
+    render(element: React.ReactNode): void
+  }
+}
