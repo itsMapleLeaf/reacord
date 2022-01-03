44cb182b41e504feb756ce979403832f08c1417f
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-03 01:56:45 -0600

    extract sendJsx function

--- packages/docs-new/src/main.tsx
+++ packages/docs-new/src/main.tsx
@@ -1,21 +1,15 @@
 import compression from "compression"
-import type { Response } from "express"
 import express from "express"
 import httpTerminator from "http-terminator"
 import pino from "pino"
 import * as React from "react"
-import { renderToStaticMarkup } from "react-dom/server.js"
+import { sendJsx } from "./helpers/send-jsx"
 import { serveFile } from "./helpers/serve-file"
 import { serveTailwindCss } from "./helpers/tailwind"
 import { Landing } from "./pages/landing"
 
 const logger = pino()
 const port = process.env.PORT || 3000
-
-function sendJsx(res: Response, jsx: React.ReactElement) {
-  res.set("Content-Type", "text/html")
-  res.send(`<!DOCTYPE html>\n${renderToStaticMarkup(jsx)}`)
-}
 
 const app = express()
   .use(compression())
