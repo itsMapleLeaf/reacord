2e4ff0f524058a1f59fb8963f8953fad4cbb312d
MapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-01-10 23:50:45 -0600

    remove detached flag

--- packages/docs/scripts/test.ts
+++ packages/docs/scripts/test.ts
@@ -3,9 +3,9 @@ import { execa } from "execa"
 import waitOn from "wait-on"
 
 await execa("pnpm", ["build"], { stdio: "inherit" })
-const app = execa("pnpm", ["start"], { stdio: "inherit", detached: true })
+const app = execa("pnpm", ["start"], { stdio: "inherit" })
 await waitOn({ resources: ["http-get://localhost:3000"] })
 await cypress.run()
-process.kill(app.pid!, "SIGKILL")
+app.kill()
 // eslint-disable-next-line unicorn/no-process-exit
 process.exit(0)
