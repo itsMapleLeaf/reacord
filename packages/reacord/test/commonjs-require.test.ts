c35c32bdddb5e6828f1d3c7245570cadad174e6c
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-07-09 14:54:27 -0500

    fix cjs require

---
+++ packages/reacord/test/commonjs-require.test.ts
@@ -0,0 +1,12 @@
+import { spawnSync } from "node:child_process"
+import { createRequire } from "node:module"
+import { beforeAll, expect, test } from "vitest"
+
+beforeAll(() => {
+  spawnSync("pnpm", ["run", "build"])
+})
+
+test("can require commonjs", () => {
+  const require = createRequire(import.meta.url)
+  expect(() => require("../dist/main.cjs")).not.toThrow()
+})
