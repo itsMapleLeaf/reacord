55b5072e1b9fc2593e20c926987ea8c457460c01
itsMapleLeaf <19603573+itsMapleLeaf@users.noreply.github.com>
2022-08-05 23:48:52 -0500

    run local build script

--- packages/reacord/test/commonjs-require.test.ts
+++ packages/reacord/test/commonjs-require.test.ts
@@ -1,9 +1,13 @@
 import { spawnSync } from "node:child_process"
 import { createRequire } from "node:module"
+import { join } from "node:path"
+import { fileURLToPath } from "node:url"
 import { beforeAll, expect, test } from "vitest"
 
 beforeAll(() => {
-  spawnSync("pnpm", ["run", "build"])
+  spawnSync("pnpm", ["run", "build"], {
+    cwd: join(fileURLToPath(import.meta.url), ".."),
+  })
 })
 
 test("can require commonjs", () => {
